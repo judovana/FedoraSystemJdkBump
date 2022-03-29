d45d0d3584fbe4efc3e895cc6a0398828022aaf7
Jiri Vanek <jvanek@redhat.com>
2022-03-29 15:45:34 +0200

    The repoqueryes may be skipped

--- scritps/listPkgs/recursiveGlue.sh
+++ scritps/listPkgs/recursiveGlue.sh
@@ -26,32 +26,35 @@ function filter2by1() {
   rm ${f2}.backup
 }
 
+if [ "x$SCANNED" = "xtrue" ] ; then
+  echo "skipping all repoquery, processing existing tierX and allWhatever files:"
+else
+  # summ up all. that wil be used fore i686 leaves and for final statistics
+  # note, that this do not honor the complex repos work as listJavaDependentPkg.sh does. But that do nto hurt at all...
+  dnf list all > all.nvras
+  cat all.nvras | grep -v "\.noarch" > all-archfull.nvras
+  cat all.nvras | grep -e "\.noarch" > all-noarch.nvras
+  cat all.nvras | grep -e "\.i686" > all-multilib.nvras
 
-# summ up all. that wil be used fore i686 leaves and for final statistics
-# note, that this do not honor the complex repos work as listJavaDependentPkg.sh does. But that do nto hurt at all...
-dnf list all > all.nvras
-cat all.nvras | grep -v "\.noarch" > all-archfull.nvras
-cat all.nvras | grep -e "\.noarch" > all-noarch.nvras
-cat all.nvras | grep -e "\.i686" > all-multilib.nvras
+  # find transitive depndencies 
+  # will skip check on dead packages and subpackages in nvfrsToNames.sh
+  export SKIP_CHECK=true;
 
-# find transitive depndencies 
-# will skip check on dead packages and subpackages in nvfrsToNames.sh
-export SKIP_CHECK=true;
+  # initial load, all what depends on java, mvn and friends in 
+  # build or runtime. result is all.jbump
+  cat $SCRIPT_DIR/listJavaDependentPkgs.sh | grep interestingDeps=\" | sed "s/.*interestingDeps=\"//" | sed "s/\".*//" | sed "s/\s\+/\n/g" >  tier0.names
 
-# initial load, all what depends on java, mvn and friends in 
-# build or runtime. result is all.jbump
-cat $SCRIPT_DIR/listJavaDependentPkgs.sh | grep interestingDeps=\" | sed "s/.*interestingDeps=\"//" | sed "s/\".*//" | sed "s/\s\+/\n/g" >  tier0.names
-
-# it may happen, that (eg by incorrect spec file) that also transitive depndence is included as top level depndence
-# eg something requires ant and java. However and already requires java, so suddenly we will have java deps listed in all tier, which we do not want
-for x in `seq 1 11` ; do
-  base=$(($x-1))
-  sh $SCRIPT_DIR/listJavaDependentPkgs.sh tier$base.names
-  cat all.jbump | sh $SCRIPT_DIR/nvfrsToNames.sh >  tier$x.names
-  cp tier$x.names tier$x-all.names # jsut for case...
-  # now  remove all already walked through guys. On tier 1 it do nearly nothing (but eg removal of ant and java put next walk to less then half).As conseqence, on two and up it removes huge numbers
-  for y in `seq 0 $base` ; do
-    filter2by1 tier$y.names tier$x.names
+  # it may happen, that (eg by incorrect spec file) that also transitive depndence is included as top level depndence
+  # eg something requires ant and java. However and already requires java, so suddenly we will have java deps listed in all tier, which we do not want
+  for x in `seq 1 11` ; do
+    base=$(($x-1))
+    sh $SCRIPT_DIR/listJavaDependentPkgs.sh tier$base.names
+    cat all.jbump | sh $SCRIPT_DIR/nvfrsToNames.sh >  tier$x.names
+    cp tier$x.names tier$x-all.names # jsut for case...
+    # now  remove all already walked through guys. On tier 1 it do nearly nothing (but eg removal of ant and java put next walk to less then half).As conseqence, on two and up it removes huge numbers
+    for y in `seq 0 $base` ; do
+      filter2by1 tier$y.names tier$x.names
+    done
+    cp all.jbump all$x.names # jsut for case...
   done
-  cp all.jbump all$x.names # jsut for case...
-done
+fi
