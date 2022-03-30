b0043261ce22ffccb32de78e934ad302140cd4f6
Jiri Vanek <jvanek@redhat.com>
2022-03-30 17:18:25 +0200

    Changed initial set of base requirements

--- scritps/listPkgs/recursiveGlue.sh
+++ scritps/listPkgs/recursiveGlue.sh
@@ -31,7 +31,7 @@ if [ "x$SCANNED" = "xtrue" ] ; then
 else
   # summ up all. that wil be used fore i686 leaves and for final statistics
   # note, that this do not honor the complex repos work as listJavaDependentPkg.sh does. But that do nto hurt at all...
-  dnf list all > all.nvras
+  dnf list all  | sed "s/\s.*//g" > all.nvras
   cat all.nvras | grep -v "\.noarch" > all-archfull.nvras
   cat all.nvras | grep -e "\.noarch" > all-noarch.nvras
   cat all.nvras | grep -e "\.i686" > all-multilib.nvras
@@ -39,15 +39,107 @@ else
   # find transitive depndencies 
   # will skip check on dead packages and subpackages in nvfrsToNames.sh
   export SKIP_CHECK=true;
+  export CHART=$PWD/edges;
+
+  rm -rf $CHART
+  mkdir  $CHART
 
   # initial load, all what depends on java, mvn and friends in 
   # build or runtime. result is all.jbump
-  cat $SCRIPT_DIR/listJavaDependentPkgs.sh | grep interestingDeps=\" | sed "s/.*interestingDeps=\"//" | sed "s/\".*//" | sed "s/\s\+/\n/g" >  tier0.names
+  # cat $SCRIPT_DIR/listJavaDependentPkgs.sh | grep interestingDeps=\" | sed "s/.*interestingDeps=\"//" | sed "s/\".*//" | sed "s/\s\+/\n/g" >  tier0.names
+  echo "java
+java-17
+java-17-openjdk
+java-openjdk
+jre
+jre-17
+jre-17-openjdk
+jre-openjdk
+java-17-demo
+java-17-openjdk-demo
+java-demo
+java-openjdk-demo
+java-17-devel
+java-17-openjdk-devel
+java-devel
+java-devel-openjdk
+java-sdk
+java-sdk-17
+java-sdk-17-openjdk
+java-sdk-openjdk
+java-17-headless
+java-17-openjdk-headless
+java-headless
+java-openjdk-headless
+jre-17-headless
+jre-17-openjdk-headless
+jre-headless
+jre-openjdk-headless
+libjava.so
+libjsig.so
+libjvm.so
+libjvm.so
+libsyslookup.so
+libverify.so
+java-17-javadoc
+java-17-openjdk-javadoc
+java-javadoc
+java-17-openjdk-javadoc-zip
+java-17-jmods
+java-17-openjdk-jmods
+java-jmods
+java-17-openjdk-src
+java-17-src
+java-openjdk-src
+java-src
+java-11
+java-11-openjdk
+jre-11
+jre-11-openjdk
+java-11-demo
+java-11-openjdk-demo
+java-11-devel
+java-11-openjdk-devel
+java-sdk-11
+java-sdk-11-openjdk
+java-11-headless
+java-11-openjdk-headless
+jre-11-headless
+jre-11-openjdk-headless
+java-11-javadoc
+java-11-openjdk-javadoc
+java-11-openjdk-javadoc-zip
+java-11-jmods
+java-11-openjdk-jmods
+java-11-openjdk-src
+java-11-src
+java-1.8.0
+java-1.8.0-openjdk
+jre-1.8.0
+jre-1.8.0-openjdk
+java-1.8.0-demo
+java-1.8.0-openjdk-demo
+java-1.8.0-devel
+java-1.8.0-openjdk-devel
+java-sdk-1.8.0
+java-sdk-1.8.0-openjdk
+java-1.8.0-headless
+java-1.8.0-openjdk-headless
+jre-1.8.0-headless
+jre-1.8.0-openjdk-headless
+java-1.8.0-javadoc
+java-1.8.0-openjdk-javadoc
+java-1.8.0-openjdk-javadoc-zip
+java-1.8.0-jmods
+java-1.8.0-openjdk-jmods
+java-1.8.0-openjdk-src
+java-1.8.0-src" >  tier0.names
 
   # it may happen, that (eg by incorrect spec file) that also transitive depndence is included as top level depndence
   # eg something requires ant and java. However and already requires java, so suddenly we will have java deps listed in all tier, which we do not want
   for x in `seq 1 11` ; do
     base=$(($x-1))
+    export CHART_TIER=$x
     sh $SCRIPT_DIR/listJavaDependentPkgs.sh tier$base.names
     cat all.jbump | sh $SCRIPT_DIR/nvfrsToNames.sh >  tier$x.names
     cp tier$x.names tier$x-all.names # jsut for case...
