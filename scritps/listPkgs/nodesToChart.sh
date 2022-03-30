f43258890ee3b0c96dc58afcb41ede9b471db336
Jiri Vanek <jvanek@redhat.com>
2022-03-30 19:44:23 +0200

    Added basic convert to dot files

---
+++ scritps/listPkgs/nodesToChart.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+if [ "$1" == "" -o  "$1" == "s"  ] ; then 
+  echo "strict graph { "
+  ls | sed "s/~is~req~by~/\" -- \"/g" | sed "s/^/\"/g" | sed "s/$/\"/g"
+  echo "}"
+else
+  echo "digraph graph {"
+  ls | sed "s/~is~req~by~/\" -> \"/g" | sed "s/^/\"/g" | sed "s/$/\"/g"
+  echo "}"
+fi
