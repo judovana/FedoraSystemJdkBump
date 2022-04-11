e67f1fc9e0be955a24e4834ad072a510817f15d2
Jiri Vanek <jvanek@redhat.com>
2022-04-11 16:46:30 +0200

    was pulled by enahcned for "recursive" variant"

--- scritps/listPkgs/wasPulledBy.sh
+++ scritps/listPkgs/wasPulledBy.sh
@@ -19,6 +19,30 @@ function readOrigin() {
   done
 }
 
-readOrigin ${@}
-
+# there are recursive depndencies unluckily...
+# so one would need to coutn anyway
+# also we never reached deeper then 5...
+# long story short to lazy to do this proeprly
+if [ "x$RECURSIVE" == "xtrue" ] ; then
+  for orig in "$@" ; do
+    current=`readOrigin $orig`
+    for x1 in  $current ; do
+      current1=`readOrigin $x1`
+      for x2 in  $current1 ; do
+        current2=`readOrigin $x2`
+        for x3 in  $current2 ; do
+          current3=`readOrigin $x3`
+          for x4 in  $current3 ; do
+            current4=`readOrigin $x4`
+            for x5 in  $current4 ; do
+              echo "$x5 <- $x4 <- $x3 <- $x2 <- $x1 <- $orig"
+            done
+          done
+        done
+      done
+    done
+  done
+else
+  readOrigin ${@}
+fi
 
