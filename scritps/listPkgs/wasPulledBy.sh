84dc637c650c8a353dfa3fc630d235a29ad7dc0d
Jiri Vanek <jvanek@redhat.com>
2022-04-11 13:51:54 +0200

    wasPulledBy now accept also single pkg as attribute

--- scritps/listPkgs/wasPulledBy.sh
+++ scritps/listPkgs/wasPulledBy.sh
@@ -6,10 +6,14 @@ else
   DEFAULT_INPUT_FILE=
 fi
 for file in "$@" $DEFAULT_INPUT_FILE ; do
-  while IFS= read -r line; do
-    echo "* $line was pulled in by:"
-    ls . | grep ~$line$ | sed "s/~is~req~by~.*//"
-  done < "$file"
+  if [ -e $file ] ; then
+    while IFS= read -r line; do
+      echo "* $line was pulled in by:"
+      ls . | grep ~$line$ | sed "s/~is~req~by~.*//"
+    done < "$file"
+  else
+    ls . | grep ~$file$ | sed "s/~is~req~by~.*//"
+  fi
 done
 
 
