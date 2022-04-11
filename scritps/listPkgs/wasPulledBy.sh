c1c857938972576a3ba95be22df08b5df3c65bea
Jiri Vanek <jvanek@redhat.com>
2022-04-11 13:54:14 +0200

    wasPulledBy changed to function

--- scritps/listPkgs/wasPulledBy.sh
+++ scritps/listPkgs/wasPulledBy.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-if [ $# = 0 ] ; then
-  DEFAULT_INPUT_FILE=/dev/stdin
-else
-  DEFAULT_INPUT_FILE=
-fi
-for file in "$@" $DEFAULT_INPUT_FILE ; do
-  if [ -e $file ] ; then
-    while IFS= read -r line; do
-      echo "* $line was pulled in by:"
-      ls . | grep ~$line$ | sed "s/~is~req~by~.*//"
-    done < "$file"
+
+function readOrigin() {
+  if [ $# = 0 ] ; then
+    DEFAULT_INPUT_FILE=/dev/stdin
   else
-    ls . | grep ~$file$ | sed "s/~is~req~by~.*//"
+    DEFAULT_INPUT_FILE=
   fi
-done
+  for file in "$@" $DEFAULT_INPUT_FILE ; do
+    if [ -e $file ] ; then
+      while IFS= read -r line; do
+        echo "* $line was pulled in by:"
+        ls . | grep ~$line$ | sed "s/~is~req~by~.*//"
+      done < "$file"
+    else
+      ls . | grep ~$file$ | sed "s/~is~req~by~.*//"
+    fi
+  done
+}
+
+readOrigin ${@}
 
 
